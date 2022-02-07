4e093a9076584faf8e102dcc230ff9e0d3a739af
rksgr <r_kjha@yahoo.com>
2022-02-07 15:16:43 +0530

    Added: class containing main method

---
+++ src/main/java/com/example/springemployeepayroll/SpringEmployeePayrollApplication.java
@@ -0,0 +1,13 @@
+package com.example.springemployeepayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmployeePayrollApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEmployeePayrollApplication.class, args);
+    }
+
+}
