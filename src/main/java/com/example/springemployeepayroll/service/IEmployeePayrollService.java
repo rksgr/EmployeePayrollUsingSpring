b58df01d317567e903a121b8a9485e716842d924
rksgr <r_kjha@yahoo.com>
2022-02-07 15:13:35 +0530

    Added: Interface for EmployeePayrollService

---
+++ src/main/java/com/example/springemployeepayroll/service/IEmployeePayrollService.java
@@ -0,0 +1,11 @@
+package com.example.springemployeepayroll.service;
+
+import com.example.springemployeepayroll.Entity.EmployeePayrollData;
+
+import java.util.List;
+
+public interface IEmployeePayrollService {
+    List<EmployeePayrollData> getEmployeePayrollData();
+
+
+}
