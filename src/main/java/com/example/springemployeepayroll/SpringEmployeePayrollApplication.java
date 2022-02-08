91e7d93413149b714e4cdc50eb596af7ac555393
rksgr <r_kjha@yahoo.com>
2022-02-09 05:05:15 +0530

    Added: Display the environment of the application

--- src/main/java/com/example/springemployeepayroll/SpringEmployeePayrollApplication.java
+++ src/main/java/com/example/springemployeepayroll/SpringEmployeePayrollApplication.java
@@ -3,15 +3,18 @@ package com.example.springemployeepayroll;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
 public class SpringEmployeePayrollApplication {
 
+    /**UC3 : Determine the logging level, logging to console or file , logging patterns, etc. based on
+                this application running in Dev, Staging or production**/
     public static void main(String[] args) {
-
-        SpringApplication.run(SpringEmployeePayrollApplication.class, args);
-        log.info("Employee Payroll App started.");
+        ApplicationContext context = SpringApplication.run(SpringEmployeePayrollApplication.class, args);
+        // Logging the environment from appropriate application property
+        log.info("Employee Payroll App started in {} environment",context.getEnvironment().getProperty("environment"));
     }
 
 }
