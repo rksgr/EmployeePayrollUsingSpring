dc5bca4a4742aae8d390d34db736888f34e500d0
rksgr <r_kjha@yahoo.com>
2022-02-09 06:01:23 +0530

    Refactored: Main method to log the datasource username

--- src/main/java/com/example/springemployeepayroll/SpringEmployeePayrollApplication.java
+++ src/main/java/com/example/springemployeepayroll/SpringEmployeePayrollApplication.java
@@ -9,12 +9,13 @@ import org.springframework.context.ApplicationContext;
 @Slf4j
 public class SpringEmployeePayrollApplication {
 
-    /**UC3 : Determine the logging level, logging to console or file , logging patterns, etc. based on
-                this application running in Dev, Staging or production**/
+    /**UC4 : Database setting as Environment Variable **/
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(SpringEmployeePayrollApplication.class, args);
         // Logging the environment from appropriate application property
         log.info("Employee Payroll App started in {} environment",context.getEnvironment().getProperty("environment"));
-    }
 
+        // Log the data source UserName
+        log.info("Employee payroll DB user is {}",context.getEnvironment().getProperty("spring.datasource.username"));
+    }
 }
