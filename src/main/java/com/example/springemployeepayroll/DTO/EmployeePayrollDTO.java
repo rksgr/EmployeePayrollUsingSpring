fa7fdcbfc4facb82df0814b518bad38d05f9985e
rksgr <r_kjha@yahoo.com>
2022-02-10 10:47:30 +0530

    Refactored: Validators for name and salary

--- src/main/java/com/example/springemployeepayroll/DTO/EmployeePayrollDTO.java
+++ src/main/java/com/example/springemployeepayroll/DTO/EmployeePayrollDTO.java
@@ -1,7 +1,16 @@
 package com.example.springemployeepayroll.DTO;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 public class EmployeePayrollDTO {
+
+    @NotEmpty(message="Employee name cannot be null")
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name invalid")
     public String name;
+
+    @Min(value=5000,message="Salary cannot be less than 5000")
     public long salary;
 
     public EmployeePayrollDTO(String name, long salary) {
