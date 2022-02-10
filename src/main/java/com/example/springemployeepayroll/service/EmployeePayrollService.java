02f888fcc8f78ebef438ac0f0fb62c0d8aede84d
rksgr <r_kjha@yahoo.com>
2022-02-10 10:48:46 +0530

    Refactored: Service layer to perform the task of CRUD operation on Model class

--- src/main/java/com/example/springemployeepayroll/service/EmployeePayrollService.java
+++ src/main/java/com/example/springemployeepayroll/service/EmployeePayrollService.java
@@ -9,10 +9,36 @@ import java.util.List;
 
 @Service
 public class EmployeePayrollService implements IEmployeePayrollService{
+    // Store the list in the RAM
+    private List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+
+
     @Override
     public List<EmployeePayrollData> getEmployeePayrollData(){
-        List<EmployeePayrollData> employeePayrollDataList = new ArrayList<>();
-        employeePayrollDataList.add(new EmployeePayrollData(1, new EmployeePayrollDTO("Altaf",5000)));
-        return employeePayrollDataList;
+        return employeePayrollList;
+    }
+
+    @Override
+    public EmployeePayrollData getEmployeePayrollDataById(int empId){
+        return employeePayrollList.get(empId-1);
+    }
+    @Override
+    public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO employeePayrollDTO){
+        EmployeePayrollData employeePayrollData = null;
+        employeePayrollData = new EmployeePayrollData(1, employeePayrollDTO);
+        employeePayrollList.add(employeePayrollData);
+        return employeePayrollData;
+    }
+    @Override
+    public EmployeePayrollData updateEmployeePayrollData(int empId, EmployeePayrollDTO employeePayrollDTO){
+        EmployeePayrollData employeePayrollData = this.getEmployeePayrollDataById(empId);
+        employeePayrollData.setName(employeePayrollDTO.name);
+        employeePayrollData.setSalary(employeePayrollDTO.salary);
+        employeePayrollList.set(empId-1, employeePayrollData);
+        return employeePayrollData;
+    }
+    @Override
+    public void deleteEmployeePayrollData(int empId){
+        employeePayrollList.remove(empId-1);
     }
 }
