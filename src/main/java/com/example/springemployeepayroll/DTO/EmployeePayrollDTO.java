1617bcf737e63e4a906a01647801675f09407c67
rksgr <r_kjha@yahoo.com>
2022-02-15 18:25:41 +0530

    Refactored: Included validation for different fields

--- src/main/java/com/example/springemployeepayroll/DTO/EmployeePayrollDTO.java
+++ src/main/java/com/example/springemployeepayroll/DTO/EmployeePayrollDTO.java
@@ -1,7 +1,6 @@
 package com.example.springemployeepayroll.DTO;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.*;
 import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 import java.util.List;
@@ -18,12 +17,22 @@ public @ToString class EmployeePayrollDTO {
 
     @Min(value=5000,message="Salary cannot be less than 5000")
     public long salary;
+
+    @Pattern(regexp="male|female",message = "Gender needs to be either male or female")
     public String gender;
 
     @JsonFormat(pattern = "dd MMM yyyy")
+    @NotNull(message = "Start date cannot be empty")
+    @PastOrPresent(message = "startDate could be either today's date or any past date")
     public LocalDate startDate;
+
+    @NotBlank(message= "Note cannot be left blank")
     public String note;
+
+    @NotBlank(message = "ProfilePic cannot be empty")
     public String profilePic;
+
+    @NotNull(message = "Department cannot be left empty")
     public List<String> departments;
 
     public EmployeePayrollDTO(String name, long salary) {
