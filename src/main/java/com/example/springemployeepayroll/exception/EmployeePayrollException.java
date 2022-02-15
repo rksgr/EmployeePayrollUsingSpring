79f0f02a38ddcca4ed0e87da50432cbde70a48af
rksgr <r_kjha@yahoo.com>
2022-02-15 20:56:35 +0530

    Refactored: constructor added

--- src/main/java/com/example/springemployeepayroll/exception/EmployeePayrollException.java
+++ src/main/java/com/example/springemployeepayroll/exception/EmployeePayrollException.java
@@ -1,4 +1,7 @@
 package com.example.springemployeepayroll.exception;
 
-public class EmployeePayrollException extends Throwable {
+public class EmployeePayrollException extends RuntimeException {
+    public EmployeePayrollException(String message){
+        super(message);
+    }
 }
---
+++ src/main/java/com/example/springemployeepayroll/repository/EmployeePayrollRepository.java
@@ -0,0 +1,2 @@
+package com.example.springemployeepayroll.repository;public interface EmployeePayrollRepository {
+}
