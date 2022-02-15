569b8a7654f9c7b4bf7073e2bf5df0d416cdf9f5
rksgr <r_kjha@yahoo.com>
2022-02-15 20:59:38 +0530

    Refactored: Annotations to create two tables out of fields of the EmployeePayroll Data class, employee_payroll and employee_department

--- src/main/java/com/example/springemployeepayroll/Entity/EmployeePayrollData.java
+++ src/main/java/com/example/springemployeepayroll/Entity/EmployeePayrollData.java
@@ -5,29 +5,49 @@ import lombok.Data;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.persistence.Entity;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
-@Data
-public class EmployeePayrollData {
+import javax.persistence.JoinColumn;
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
+
+@Entity
+@Table(name="employee_payroll")
+public @Data class EmployeePayrollData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="employee_id")
     private long employeeId;
 
     @Pattern(regexp = "^[A-Z]{1}[a-zA-Z]{2,}$", message = "Employee name invalid")
+    @Column(name="name")
     private String name;
+
     @Min(value=5000,message="Salary cannot be less than 5000")
+    @Column()
     private long salary;
 
     private String gender;
     private LocalDate startDate;
     private String note;
     private String profilePic;
+
+    // Another table created since one to many relationship is there
+    @ElementCollection
+    @CollectionTable(name="employee_department", joinColumns=@JoinColumn(name="id"))
+    @Column(name="department")
     private List<String> departments;
 
 
     public EmployeePayrollData(){}
 
-    public EmployeePayrollData(long employeeId, EmployeePayrollDTO employeePayrollDTO) {
-        this.employeeId = employeeId;
+    public EmployeePayrollData(EmployeePayrollDTO employeePayrollDTO) {
         this.updateEmployeePayrollData(employeePayrollDTO);
     }
 
