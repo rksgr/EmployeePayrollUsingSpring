ac6735ac8615d1b4e5d66c49add3d05a09725236
rksgr <r_kjha@yahoo.com>
2022-02-15 21:03:11 +0530

    class to extend JpaRepository

--- src/main/java/com/example/springemployeepayroll/repository/EmployeePayrollRepository.java
+++ src/main/java/com/example/springemployeepayroll/repository/EmployeePayrollRepository.java
@@ -1,2 +1,11 @@
-package com.example.springemployeepayroll.repository;public interface EmployeePayrollRepository {
+package com.example.springemployeepayroll.repository;
+
+import com.example.springemployeepayroll.Entity.EmployeePayrollData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+
+
+public interface EmployeePayrollRepository extends JpaRepository<EmployeePayrollData, Integer> {
+
 }
