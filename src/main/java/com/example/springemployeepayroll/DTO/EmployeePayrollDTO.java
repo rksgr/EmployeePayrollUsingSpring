d96289ef9ae1127dd72c1f095d63858ea8fa3485
rksgr <r_kjha@yahoo.com>
2022-02-15 17:52:42 +0530

    Refcatored: Additional fields profilepic,strtdate,gender, note, departments, added along with @ToString annotation

--- src/main/java/com/example/springemployeepayroll/DTO/EmployeePayrollDTO.java
+++ src/main/java/com/example/springemployeepayroll/DTO/EmployeePayrollDTO.java
@@ -3,26 +3,31 @@ package com.example.springemployeepayroll.DTO;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
+import java.time.LocalDate;
+import java.util.List;
 
-public class EmployeePayrollDTO {
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.ToString;
+import java.util.regex.*;
 
-    @NotEmpty(message="Employee name cannot be null")
-    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name invalid")
+public @ToString class EmployeePayrollDTO {
+
+    @Pattern(regexp = "^[A-Z]{1,}[a-zA-Z]{2,}$", message = "Employee name invalid")
+    @NotEmpty(message="Cannot be left blank")
     public String name;
 
     @Min(value=5000,message="Salary cannot be less than 5000")
     public long salary;
+    public String gender;
+
+    @JsonFormat(pattern = "dd MMM yyyy")
+    public LocalDate startDate;
+    public String note;
+    public String profilePic;
+    public List<String> departments;
 
     public EmployeePayrollDTO(String name, long salary) {
         this.name = name;
         this.salary = salary;
-    }
-
-    @Override
-    public String toString() {
-        return "EmployeePayrollDTO{" +
-                "name='" + name + '\'' +
-                ", salary=" + salary +
-                '}';
     }
 }
